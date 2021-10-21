42d3e06e8035883e0ebb25b3eb6382b10950f420
gamerraming <gamerraming@gmail.com>
2021-10-21 21:55:52 +0700

    update 2 forms

---
+++ DACN/DACN/Models/nhaxuatban.cs
@@ -0,0 +1,28 @@
+namespace DACN.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("nhaxuatban")]
+    public partial class nhaxuatban
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public nhaxuatban()
+        {
+            saches = new HashSet<sach>();
+        }
+
+        [Key]
+        [StringLength(10)]
+        public string manhaxuatban { get; set; }
+
+        [StringLength(50)]
+        public string tennhaxuatban { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<sach> saches { get; set; }
+    }
+}
