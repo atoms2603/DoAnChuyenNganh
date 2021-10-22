29245d08bfcf3a745d187890fa08fc1de9d8edb8
gamerraming <gamerraming@gmail.com>
2021-10-22 13:15:40 +0700

    add form xoa, form them, va cac form khac.

--- DACN/DACN/Controllers/HomeController.cs
+++ DACN/DACN/Controllers/HomeController.cs
@@ -18,5 +18,39 @@ namespace DACN.Controllers
         {
             return View(db.saches.ToList());
         }
+        public ActionResult formThemSach()
+        {
+            ViewBag.dsTheLoai = db.theloais.ToList();
+            ViewBag.dsTacGia = db.tacgias.ToList();
+            ViewBag.dsNhaXuatBan = db.nhaxuatbans.ToList();
+            return View();
+        }
+        [HttpPost]
+        public ActionResult themSach(Models.sach s)
+        {
+            if (ModelState.IsValid)
+            {
+                db.saches.Add(s);
+                db.SaveChanges();
+            }
+            return RedirectToAction("formQuanLySach");
+        }
+
+        public ActionResult formXoaSach()
+        {
+            return View(db.saches.ToList());
+        }
+        public ActionResult formQuyetDinhXoa(string id)
+        {
+            Models.sach s = db.saches.Find(id);
+            return View(s);
+        }
+        public ActionResult xoaSach(string id)
+        {
+            Models.sach s = db.saches.Find(id);
+            db.saches.Remove(s);
+            db.SaveChanges();
+            return RedirectToAction("formQuanLySach");
+        }
     }
 }
