5f404945e80673aef6e823d7e8f3e74ddcf5b504
gamerraming <gamerraming@gmail.com>
2021-10-31 14:45:04 +0700

    fix form thêm sách

--- Controllers/SachController.cs
+++ Controllers/SachController.cs
@@ -9,7 +9,8 @@ namespace QLSachOnline.Controllers
         public ActionResult QuanLySach()
         {
             ViewBag.dsSach_TG = db.sach_tacgia;
-            return View(db.sach_theloai);
+            ViewBag.dsSach_TL = db.sach_theloai;
+            return View(db.saches);
         }
         public ActionResult formThongTinChiTiet(string id)
         {
@@ -29,6 +30,43 @@ namespace QLSachOnline.Controllers
             ViewBag.dsSach_TG = db.sach_tacgia;
             ViewBag.dsNXB = db.nhaxuatbans;
             return View(db.sach_theloai);
+        }
+        [HttpPost]
+        public ActionResult themSach()
+        {
+            if (ModelState.IsValid)
+            {
+                string maSach = Request["masach"].ToString();
+                string tenSach = Request["tensach"].ToString();
+                int namxb = System.Convert.ToInt32(Request["namxuatban"].ToString());
+                string maLoai = Request["maloai"].ToString();
+                string maNXB = Request["manhaxuatban"].ToString();
+                string maTacGia = Request["matg"].ToString();
+                decimal gia = System.Convert.ToDecimal(Request["phi"].ToString());
+
+                QLSachOnline.Models.sach_theloai sach_Theloai = new Models.sach_theloai();
+                QLSachOnline.Models.sach_tacgia sach_Tacgia = new Models.sach_tacgia();
+                QLSachOnline.Models.sach sach = new Models.sach();
+
+                sach.masach = maSach;
+                sach.tensach = tenSach;
+                sach.namxuatban = namxb;
+                sach.phi = gia;
+                sach.manhaxuatban = maNXB;
+
+                sach_Theloai.maloai = maLoai;
+                sach_Theloai.masach = maSach;
+
+                sach_Tacgia.masach = maSach;
+                sach_Tacgia.matg = maTacGia;
+
+                db.saches.Add(sach);
+                db.sach_theloai.Add(sach_Theloai);
+                db.sach_tacgia.Add(sach_Tacgia);
+
+                db.SaveChanges();
+            }
+            return RedirectToAction("QuanLySach");
         }
         public ActionResult formChinhSua(string id)
         {
--- Controllers/TacGiaController.cs
+++ Controllers/TacGiaController.cs
@@ -8,26 +8,48 @@ namespace QLSachOnline.Controllers
 {
     public class TacGiaController : Controller
     {
+        private Models.QLySachOnline db = new Models.QLySachOnline();
         // GET: TacGia
         public ActionResult QuanLyTacGia()
         {
-            return View();
+            return View(db.tacgias);
         }
         public ActionResult formThemTacGia()
         {
             return View();
         }
-        public ActionResult formXoaTacGia()
+        
+        [HttpPost]
+        public ActionResult themTacGia(QLSachOnline.Models.tacgia tg)
         {
-            return View();
+            if (ModelState.IsValid)
+            {
+                db.tacgias.Add(tg);
+                db.SaveChanges();
+            }
+            return RedirectToAction("QuanLyTacGia");
         }
-        public ActionResult formChiTietTacGia()
+        public ActionResult formXoaTacGia(String id)
         {
-            return View();
+            if (db.tacgias.Find(id).sach_tacgia.Count == 0)
+                ViewBag.flagXoa = true;
+            return View(db.tacgias.Find(id));
         }
-        public ActionResult formChinhSuaTG()
+        [HttpPost]
+        public ActionResult xoaTacGia(string id)
         {
-            return View();
+            QLSachOnline.Models.tacgia tg = db.tacgias.Find(id);
+            db.tacgias.Remove(tg);
+            db.SaveChanges();
+            return RedirectToAction("QuanLyTacGia");
+        }
+        public ActionResult formChiTietTacGia(String id)
+        {
+            return View(db.tacgias.Find(id));
+        }
+        public ActionResult formChinhSuaTG(String id)
+        {
+            return View(db.tacgias.Find(id));
         }
     }
 }
