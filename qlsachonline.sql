4adb826227af1f05f718c73721391e2ba13d8932
atoms2603 <63764215+atoms2603@users.noreply.github.com>
2021-10-31 19:41:58 +0700

    update ultra sql

--- qlsachonline.sql
+++ qlsachonline.sql
@@ -58,13 +58,13 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE TABLE user_login(
+CREATE TABLE userlogin(
 	[taikhoan] [varchar](50) NOT NULL,
 	[matkhau] [varchar](50)not NULL,
 	[sdt] [varchar](10) NULL,
 	[email] [nvarchar](50)not NULL,
 	[status] [bit] null,
- CONSTRAINT [PK_user_login] PRIMARY KEY CLUSTERED 
+ CONSTRAINT [PK_userlogin] PRIMARY KEY CLUSTERED 
 (
 	[taikhoan] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
@@ -80,22 +80,36 @@ GO
 CREATE TABLE sach(
 	[masach] [varchar](10) NOT NULL,
 	[manhaxuatban] [varchar](10) NOT NULL,
-	FOREIGN KEY (manhaxuatban) REFERENCES nhaxuatban(manhaxuatban),
 	[tensach] [nvarchar](50) null,
 	[namxuatban] [int] null,
-	[sochuong] [int] null,
-	[hinhanh] [image] null,
+	[hinhanh] [nvarchar](200) null,
 	[tomtat] [nvarchar] null,
 	[phi] [money] null,
-
 
  CONSTRAINT [PK_sach] PRIMARY KEY CLUSTERED 
 (
 	[masach] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
-ALTER TABLE sach
-ALTER COLUMN tensach nvarchar(50)
+
+GO
+/****** Object:  Table [dbo].[sach]    Script Date: 18/10/2021 2:00:00 AM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE chuong(
+	[machuong] [varchar](10) NOT NULL,
+	[masach] [varchar](10) NOT NULL,
+	[tenchuong] [nvarchar](100) null,
+
+ CONSTRAINT [PK_chuong] PRIMARY KEY CLUSTERED 
+(
+	[machuong] ASC,
+	[masach]asc
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
 
 GO
 /****** Object:  Table [dbo].[tacgia]    Script Date: 18/10/2021 2:00:00 AM ******/
@@ -108,11 +122,11 @@ CREATE TABLE giaodich(
 	[taikhoan] [varchar](50) NOT NULL,
 	[masach] [varchar](10) NOT NULL,
 	[ngaygiaodich] [datetime] null,
-	FOREIGN KEY (taikhoan) REFERENCES user_login(taikhoan),
-	FOREIGN KEY (masach) REFERENCES sach(masach),
  CONSTRAINT [PK_giaodich] PRIMARY KEY CLUSTERED 
 (
-	[magd] asc
+	[magd] asc,
+	[taikhoan]asc,
+	[masach] asc
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 
@@ -122,15 +136,13 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE TABLE sach_tacgia(
-	[id] int not null IDENTITY(1,1),
+CREATE TABLE tacgiasach(
 	[masach] [varchar](10) NOT NULL ,
 	[matg] [varchar](20) NOT NULL ,
-	FOREIGN KEY (masach) REFERENCES sach(masach),
-	FOREIGN KEY (matg) REFERENCES tacgia(matg),
-CONSTRAINT [PK_sach_tacgia] PRIMARY KEY CLUSTERED 
+CONSTRAINT [PK_tacgiasach] PRIMARY KEY CLUSTERED 
 (
-	[id] asc
+	[masach] asc,
+	[matg] asc
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 
@@ -140,30 +152,91 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
-CREATE TABLE sach_theloai(
-	[id] int not null IDENTITY(1,1),
+CREATE TABLE theloaisach(
 	[masach] [varchar](10) NOT NULL,
 	[maloai] [varchar](10) NOT NULL,
-	FOREIGN KEY (masach) REFERENCES sach(masach),
-	FOREIGN KEY (maloai) REFERENCES theloai(maloai),
-CONSTRAINT [PK_sach_theloai] PRIMARY KEY CLUSTERED 
+CONSTRAINT [PK_theloaisach] PRIMARY KEY CLUSTERED 
 (
-	[id] asc
+	[masach] asc,
+	[maloai] asc
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 
 CREATE TABLE luusach(
-	[id] int not null IDENTITY(1,1),
 	[masach] [varchar](10) NOT NULL,
 	[taikhoan] [varchar](50) NOT NULL,
 	[ngayluu] [datetime] null,
-	FOREIGN KEY (masach) REFERENCES sach(masach),
-	FOREIGN KEY (taikhoan) REFERENCES user_login(taikhoan),
 CONSTRAINT [PK_luusach] PRIMARY KEY CLUSTERED 
 (
-	[id] asc
+	[masach] asc,
+	[taikhoan] asc
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
+
+GO
+ALTER TABLE [dbo].[sach]  WITH CHECK ADD  CONSTRAINT [FK_sach_nhaxuatban] FOREIGN KEY([manhaxuatban])
+REFERENCES [dbo].[nhaxuatban] ([manhaxuatban])
+GO
+ALTER TABLE [dbo].[sach] CHECK CONSTRAINT [FK_sach_nhaxuatban]
+
+GO
+ALTER TABLE [dbo].[chuong]  WITH CHECK ADD  CONSTRAINT [FK_chuong_sach] FOREIGN KEY([masach])
+REFERENCES [dbo].[sach] ([masach])
+GO
+ALTER TABLE [dbo].[chuong] CHECK CONSTRAINT [FK_chuong_sach]
+
+GO
+ALTER TABLE [dbo].[tacgiasach]  WITH CHECK ADD  CONSTRAINT [FK_tacgiasach_sach] FOREIGN KEY([masach])
+REFERENCES [dbo].[sach] ([masach])
+GO
+ALTER TABLE [dbo].[tacgiasach] CHECK CONSTRAINT [FK_tacgiasach_sach]
+
+
+GO
+ALTER TABLE [dbo].[tacgiasach]  WITH CHECK ADD  CONSTRAINT [FK_tacgiasach_tacgia] FOREIGN KEY([matg])
+REFERENCES [dbo].[tacgia] ([matg])
+GO
+ALTER TABLE [dbo].[tacgiasach] CHECK CONSTRAINT [FK_tacgiasach_tacgia]
+
+GO
+ALTER TABLE [dbo].[theloaisach]  WITH CHECK ADD  CONSTRAINT [FK_theloaisach_sach] FOREIGN KEY([masach])
+REFERENCES [dbo].[sach] ([masach])
+GO
+ALTER TABLE [dbo].[theloaisach] CHECK CONSTRAINT [FK_theloaisach_sach]
+
+
+GO
+ALTER TABLE [dbo].[theloaisach]  WITH CHECK ADD  CONSTRAINT [FK_theloaisach_theloai] FOREIGN KEY([maloai])
+REFERENCES [dbo].[theloai] ([maloai])
+GO
+ALTER TABLE [dbo].[theloaisach] CHECK CONSTRAINT [FK_theloaisach_theloai]
+
+GO
+ALTER TABLE [dbo].[giaodich]  WITH CHECK ADD  CONSTRAINT [FK_giaodich_userlogin] FOREIGN KEY([taikhoan])
+REFERENCES [dbo].[userlogin] ([taikhoan])
+GO
+ALTER TABLE [dbo].[giaodich] CHECK CONSTRAINT [FK_giaodich_userlogin]
+
+GO
+ALTER TABLE [dbo].[giaodich]  WITH CHECK ADD  CONSTRAINT [FK_giaodich_sach] FOREIGN KEY([masach])
+REFERENCES [dbo].[sach] ([masach])
+GO
+ALTER TABLE [dbo].[giaodich] CHECK CONSTRAINT [FK_giaodich_sach]
+
+
+GO
+ALTER TABLE [dbo].[luusach]  WITH CHECK ADD  CONSTRAINT [FK_luusach_userlogin] FOREIGN KEY([taikhoan])
+REFERENCES [dbo].[userlogin] ([taikhoan])
+GO
+ALTER TABLE [dbo].[luusach] CHECK CONSTRAINT [FK_luusach_userlogin]
+
+GO
+ALTER TABLE [dbo].[luusach]  WITH CHECK ADD  CONSTRAINT [FK_luusach_sach] FOREIGN KEY([masach])
+REFERENCES [dbo].[sach] ([masach])
+GO
+ALTER TABLE [dbo].[luusach] CHECK CONSTRAINT [FK_luusach_sach]
+
+
 
 insert into nhaxuatban values ('NXB001',N'Kim Đồng',null)
 insert into nhaxuatban values ('NXB002',N'Nhà xuất bản trẻ',null)
@@ -192,51 +265,142 @@ insert into theloai values ('TL010',N'Lịch sử',null)
 insert into theloai values ('TL011',N'Công nghệ',null)
 insert into theloai values ('TL012',N'Tôn giáo',null)
 
-insert into sach values('S001','NXB001',N'Hoa rơi cửa phật',2015,10,null,null,0)
-insert into sach values('S002','NXB001',N'Cây cam ngọt của tôi',2018,20,null,null,0) 
-insert into sach values('S003','NXB002',N'Con đường tu tiên của tôi',2019,50,null,null,100000) 
-insert into sach values('S004','NXB002',N'Hoa vẫn nở mỗi ngày',2020,100,null,null,50000)
-insert into sach values('S005','NXB001',N'Vạn sự tùy duyên',2017,30,null,null,0)
-insert into sach values('S006','NXB004',N'Đắc nhân tâm',2021,65,null,null,150000)
-insert into sach values('S007','NXB003',N'Muôn kiếp nhân sinh',2015,10,null,null,0)
-insert into sach values('S008','NXB005',N'Nóng giận là bản năng',2021,20,null,null,0) 
-insert into sach values('S009','NXB006',N'Nhà đầu tư thông minh',2019,50,null,null,100000) 
-insert into sach values('S010','NXB005',N'Hai số phận',2020,100,null,null,50000)
-insert into sach values('S011','NXB004',N'Hoàng tử bé',2019,30,null,null,0)
-insert into sach values('S012','NXB003',N'Tâm lý học',2021,10,null,null,150000)
+insert into sach values('S001','NXB001',N'Hoa rơi cửa phật',2015,null,null,0)
+insert into sach values('S002','NXB001',N'Cây cam ngọt của tôi',2018,null,null,0) 
+insert into sach values('S003','NXB002',N'Con đường tu tiên của tôi',2019,null,null,100000) 
+insert into sach values('S004','NXB002',N'Hoa vẫn nở mỗi ngày',2020,null,null,50000)
+insert into sach values('S005','NXB001',N'Vạn sự tùy duyên',2017,null,null,0)
+insert into sach values('S006','NXB004',N'Đắc nhân tâm',2021,null,null,150000)
+insert into sach values('S007','NXB003',N'Muôn kiếp nhân sinh',2015,null,null,0)
+insert into sach values('S008','NXB005',N'Nóng giận là bản năng',2021,null,null,0) 
+insert into sach values('S009','NXB006',N'Nhà đầu tư thông minh',2019,null,null,100000) 
+insert into sach values('S010','NXB005',N'Hai số phận',2020,null,null,50000)
+insert into sach values('S011','NXB004',N'Hoàng tử bé',2019,null,null,0)
+insert into sach values('S012','NXB003',N'Tâm lý học',2021,null,null,150000)
 
-insert into sach_tacgia values('S001','TG001')
-insert into sach_tacgia values('S001','TG003')
-insert into sach_tacgia values('S002','TG001')
-insert into sach_tacgia values('S003','TG002')
-insert into sach_tacgia values('S004','TG002')
-insert into sach_tacgia values('S004','TG003')
-insert into sach_tacgia values('S005','TG001')
-insert into sach_tacgia values('S006','TG002')
-insert into sach_tacgia values('S007','TG005')
-insert into sach_tacgia values('S008','TG006')
-insert into sach_tacgia values('S008','TG005')
-insert into sach_tacgia values('S009','TG003')
-insert into sach_tacgia values('S010','TG004')
-insert into sach_tacgia values('S011','TG002')
-insert into sach_tacgia values('S012','TG005')
-insert into sach_tacgia values('S012','TG001')
+insert into tacgiasach values('S001','TG001')
+insert into tacgiasach values('S001','TG003')
+insert into tacgiasach values('S002','TG001')
+insert into tacgiasach values('S003','TG002')
+insert into tacgiasach values('S004','TG002')
+insert into tacgiasach values('S004','TG003')
+insert into tacgiasach values('S005','TG001')
+insert into tacgiasach values('S006','TG002')
+insert into tacgiasach values('S007','TG005')
+insert into tacgiasach values('S008','TG006')
+insert into tacgiasach values('S008','TG005')
+insert into tacgiasach values('S009','TG003')
+insert into tacgiasach values('S010','TG004')
+insert into tacgiasach values('S011','TG002')
+insert into tacgiasach values('S012','TG005')
+insert into tacgiasach values('S012','TG001')
 
 
-insert into sach_theloai values('S001','TL001')
-insert into sach_theloai values('S001','TL002')
-insert into sach_theloai values('S002','TL002')
-insert into sach_theloai values('S003','TL003')
-insert into sach_theloai values('S004','TL003')
-insert into sach_theloai values('S004','TL004')
-insert into sach_theloai values('S005','TL001')
-insert into sach_theloai values('S006','TL005')
-insert into sach_theloai values('S006','TL006')
-insert into sach_theloai values('S007','TL004')
-insert into sach_theloai values('S008','TL008')
-insert into sach_theloai values('S008','TL010')
-insert into sach_theloai values('S009','TL011')
-insert into sach_theloai values('S010','TL007')
-insert into sach_theloai values('S011','TL012')
-insert into sach_theloai values('S012','TL009')
-insert into sach_theloai values('S012','TL009')
+insert into theloaisach values('S001','TL001')
+insert into theloaisach values('S001','TL002')
+insert into theloaisach values('S002','TL002')
+insert into theloaisach values('S003','TL003')
+insert into theloaisach values('S004','TL003')
+insert into theloaisach values('S004','TL004')
+insert into theloaisach values('S005','TL001')
+insert into theloaisach values('S006','TL005')
+insert into theloaisach values('S006','TL006')
+insert into theloaisach values('S007','TL004')
+insert into theloaisach values('S008','TL008')
+insert into theloaisach values('S008','TL010')
+insert into theloaisach values('S009','TL011')
+insert into theloaisach values('S010','TL007')
+insert into theloaisach values('S011','TL012')
+insert into theloaisach values('S012','TL009')
+insert into theloaisach values('S012','TL008')
+
+insert into chuong values('C001','S001',null)
+insert into chuong values('C002','S001',null)
+insert into chuong values('C003','S001',null)
+insert into chuong values('C004','S001',null)
+insert into chuong values('C005','S001',null)
+insert into chuong values('C006','S001',null)
+
+insert into chuong values('C001','S002',null)
+insert into chuong values('C002','S002',null)
+insert into chuong values('C003','S002',null)
+insert into chuong values('C004','S002',null)
+insert into chuong values('C005','S002',null)
+insert into chuong values('C006','S002',null)
+
+insert into chuong values('C001','S003',null)
+insert into chuong values('C002','S003',null)
+insert into chuong values('C003','S003',null)
+insert into chuong values('C004','S003',null)
+insert into chuong values('C005','S003',null)
+insert into chuong values('C006','S003',null)
+
+insert into chuong values('C001','S004',null)
+insert into chuong values('C002','S004',null)
+insert into chuong values('C003','S004',null)
+insert into chuong values('C004','S004',null)
+insert into chuong values('C005','S004',null)
+insert into chuong values('C006','S004',null)
+
+insert into chuong values('C001','S005',null)
+insert into chuong values('C002','S005',null)
+insert into chuong values('C003','S005',null)
+insert into chuong values('C004','S005',null)
+insert into chuong values('C005','S005',null)
+insert into chuong values('C006','S005',null)
+
+insert into chuong values('C001','S006',null)
+insert into chuong values('C002','S006',null)
+insert into chuong values('C003','S006',null)
+insert into chuong values('C004','S006',null)
+insert into chuong values('C005','S006',null)
+insert into chuong values('C006','S006',null)
+
+insert into chuong values('C001','S007',null)
+insert into chuong values('C002','S007',null)
+insert into chuong values('C003','S007',null)
+insert into chuong values('C004','S007',null)
+insert into chuong values('C005','S007',null)
+insert into chuong values('C006','S007',null)
+
+insert into chuong values('C001','S008',null)
+insert into chuong values('C002','S008',null)
+insert into chuong values('C003','S008',null)
+insert into chuong values('C004','S008',null)
+insert into chuong values('C005','S008',null)
+insert into chuong values('C006','S008',null)
+
+insert into chuong values('C001','S009',null)
+insert into chuong values('C002','S009',null)
+insert into chuong values('C003','S009',null)
+insert into chuong values('C004','S009',null)
+insert into chuong values('C005','S009',null)
+insert into chuong values('C006','S009',null)
+
+insert into chuong values('C001','S009',null)
+insert into chuong values('C002','S009',null)
+insert into chuong values('C003','S009',null)
+insert into chuong values('C004','S009',null)
+insert into chuong values('C005','S009',null)
+insert into chuong values('C006','S009',null)
+
+insert into chuong values('C001','S010',null)
+insert into chuong values('C002','S010',null)
+insert into chuong values('C003','S010',null)
+insert into chuong values('C004','S010',null)
+insert into chuong values('C005','S010',null)
+insert into chuong values('C006','S010',null)
+
+insert into chuong values('C001','S011',null)
+insert into chuong values('C002','S011',null)
+insert into chuong values('C003','S011',null)
+insert into chuong values('C004','S011',null)
+insert into chuong values('C005','S011',null)
+insert into chuong values('C006','S011',null)
+
+insert into chuong values('C001','S012',null)
+insert into chuong values('C002','S012',null)
+insert into chuong values('C003','S012',null)
+insert into chuong values('C004','S012',null)
+insert into chuong values('C005','S012',null)
+insert into chuong values('C006','S012',null)
